20018d7013952b75229afe72601cbdcea45a3cca
AwetTsegay <89991123+AwetTsegay@users.noreply.github.com>
2022-04-15 22:23:02 +0100

    Add files via upload

---
+++ Q40.m
@@ -0,0 +1,8 @@
+vec = [5 11 2 33 -4]
+v1 = vec(2:end)
+v2 = vec(1:end-1)
+v1-v2
+
+%help diff
+ %diff Difference and approximate derivative.
+ %diff(X), for a vector X, is [X(2)-X(1)  X(3)-X(2) ... X(n)-X(n-1)].
